54ce7c3f309ccac7364d03b6e354bc9d10013105
vuthithao <thaovt6@topica.edu.vn>
2021-01-29 15:07:59 +0700

    cmd

--- call.py
+++ call.py
@@ -3,6 +3,8 @@ import wave
 import librosa
 import requests
 import numpy as np
+from difflib import SequenceMatcher
+import re
 
 chunk = 1024  # Record in chunks of 1024 samples
 sample_format = pyaudio.paInt16  # 16 bits per sample
@@ -10,6 +12,33 @@ channels = 1
 fs = 16000  # Record at 44100 samples per second
 seconds = 3
 filename = "output.wav"
+
+def similar(a, b):
+    return SequenceMatcher(None, a, b).ratio()
+
+
+patterns = {
+    '[àáảãạăắằẵặẳâầấậẫẩ]': 'a',
+    '[đ]': 'd',
+    '[èéẻẽẹêềếểễệ]': 'e',
+    '[ìíỉĩị]': 'i',
+    '[òóỏõọôồốổỗộơờớởỡợ]': 'o',
+    '[ùúủũụưừứửữự]': 'u',
+    '[ỳýỷỹỵ]': 'y'
+}
+
+def convert(text):
+    """
+    Convert from 'Tieng Viet co dau' thanh 'Tieng Viet khong dau'
+    text: input string to be converted
+    Return: string converted
+    """
+    output = text
+    for regex, replace in patterns.items():
+        output = re.sub(regex, replace, output)
+        # deal with upper case
+        output = re.sub(regex.upper(), replace.upper(), output)
+    return output
 
 def record():
     p = pyaudio.PyAudio()  # Create an interface to PortAudio
@@ -46,6 +75,11 @@ def record():
     wf.close()
     return 0
 
+with open('command.txt') as f:
+    command = f.readlines()
+command = [i.split('\n')[0] for i in command]
+
+print(command)
 while(1):
     _ = record()
     test_wavs, _ = librosa.load('output.wav', sr=16000, mono=True)
@@ -53,6 +87,11 @@ while(1):
     datadict = {'test_wavs': test_wavs}
     r = requests.post('http://103.137.4.6:4000/stt/v1', json=datadict)
     print(r.json())
+    sim = []
+    for i in command:
+        sim.append(similar(convert(r.json()['predict'].lower()), i))
+    if max(sim) > 0.5:
+        print(command[sim.index(max(sim))])
 
 
 
